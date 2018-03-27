152a2074d5eee5846fd17d323c3fced5068f0482
mirelatv <noelia.mirela@gmail.com>
2018-03-27 11:53:12 -0500

    subiendo carrusel automatico y splash sin funcion

--- src/components/SimpleSlider.js
+++ src/components/SimpleSlider.js
@@ -4,25 +4,28 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
-
-
+ 
+  
   var settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
   };
 
   const SimpleSlider = ({ movies }) => (
 
-    <Slider {...settings}>
+    <Slider {...settings} className=''>
       {movies.map(obj =>
-        <div className="col-12"
+        <div className=" "
           key={obj.name}
         // onClick={() => setSelectedBank(bank)}
         >
-          <img className="img-fluid"
+          <img className=""
             src={obj.image}
             alt={obj.name}
           />
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+
+
+
 const Splash =()=>(
-  <div>
-    <img src="" alt=""/>
+  <div className='cover col-12  d-flex align-items-center justify-content-center container'>
+    <img className='col-5  logo' src="assets/logo.png" alt="Logo"/>
   </div>
 )
 
